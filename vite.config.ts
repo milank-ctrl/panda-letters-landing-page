a94b31d7329cc52aa9eb1cddd9b3be67acb7cae0
milankocza <kocza.milan@gmail.com>
2025-12-30 19:10:28 +0100

    build for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { componentTagger } from "lovable-tagger";
 import { fileURLToPath } from "url";
 
@@ -12,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), cloudflare()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
